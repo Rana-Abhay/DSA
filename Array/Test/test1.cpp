cbe11125e65c0f68f2b34b261bda6bf825f91f3b
THERANADEV <KUMARRANAABHAY@GMAILCOM>
2026-06-17 19:04:27 +0530

    testing

--- Array/Test/test1.cpp
+++ Array/Test/test1.cpp
@@ -2,4 +2,13 @@
 using namespace std;
 int main(){
     cout<< "Hello G, Kya halchal"<< endl;
+
+    string name;
+    cout<<"Enter your name here"<<endl;
+    cin>> name;
+    cout<< "Hello " + name+ ", How are you ?";
+
+
+
+    return 0;
 }
